9fe501308bb7ea7186fa51b0bc1befd2bc16110c
Andrew Henrique <andrewhenriquef@gmail.com>
2016-11-14 17:09:38 -0200

    added de function delete to delete records from database

--- app/controllers/todos/edit.js
+++ app/controllers/todos/edit.js
@@ -1,4 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    
+    actions: {
+        editTodo: function(id){
+            var self = this;
+            var title = this.get('model.title');
+            var body = this.get('model.body');
+            var date = this.get('model.date');
+
+            this.store.findRecord('todo' ,id).then(function(todo){
+                todo.set('title', title);
+                todo.set('body', body);
+                todo.set('date', new Date(date));
+
+                // save to Firebase
+                todo.save();
+                //redirect route
+                self.transitionToRoute('todos');
+
+            });
+        },
+            deleteTodo: function(id){
+            
+            this.store.findRecord('todo' ,id).then(function(todo){
+                todo.destroyRecord();
+                self.transitionToRoute('todos');
+
+            });
+        
+        }
+    }
+
 });
